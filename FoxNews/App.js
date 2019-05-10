1924e57bd5f341b410cb7a3ba1ebf40f6364e69b
Abhishek Thapliyal <abhishek@nickelfox.com>
2019-05-10 13:55:24 +0530

    feat(Code): adding API call

--- FoxNews/App.js
+++ FoxNews/App.js
@@ -7,43 +7,49 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Platform, StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props){
+    super(props);
+    this.state = { isLoading: true }
+  }
+
+  componentDidMount() {
+      console.log("COMPONENT DID MOUNT");
+      return fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=7b1568b218554e659e942bfed4c4e20d')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.articles);
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.articles,
+        }, function(){ });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, padding:20}}>
+        <FlatList
+          data = { this.state.dataSource }
+          renderItem = {({item}) => <Text>{item.title}, {item.publishedAt}</Text>}
+          keyExtractor = {({id}, index) => id }
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
