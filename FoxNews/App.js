15212d45c45b824992f8a54b75f79e99eba3b2ce
Abhishek Thapliyal <abhishek@nickelfox.com>
2019-05-10 19:06:23 +0530

    feat(UI): API and Table with custom cell

--- FoxNews/App.js
+++ FoxNews/App.js
@@ -7,9 +7,25 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, ActivityIndicator, Image} from 'react-native';
 
 type Props = {};
+
+const CustomListItem = ({ title, description, imageUrl }) => (
+    <View style = {{flex: 1,
+                   justifyContent: 'space-between',
+                   flexDirection: 'column',
+                   alignItems: 'stretch',
+                   borderRadius: 5,
+                    borderWidth: 1,
+                    borderColor: '#d6d7da', marginBottom: 10}}>
+        <Image style = {{height: 200, resizeMode: 'contain'}}
+            source = {{uri: imageUrl}}/>
+        <Text style = {{fontSize: 14, fontWeight: "600", marginBottom: 5, marginTop: 10}}>{title}</Text>
+        <Text style = {{fontSize: 12, marginBottom: 10}}>{description}</Text>
+    </View>
+);
+
 export default class App extends Component<Props> {
 
   constructor(props){
@@ -43,13 +59,51 @@ export default class App extends Component<Props> {
     }
 
     return(
-      <View style={{flex: 1, padding:20}}>
+      <View style={{flex: 1, paddingTop: 25, paddingLeft: 15, paddingRight: 15 , paddingBottom: 20}}>
         <FlatList
           data = { this.state.dataSource }
-          renderItem = {({item}) => <Text>{item.title}, {item.publishedAt}</Text>}
+          renderItem = {({item}) => <CustomListItem
+                                    title={item.title}
+                                    description={item.description}
+                                    imageUrl={item.urlToImage}/>}
           keyExtractor = {({id}, index) => id }
         />
       </View>
     );
   }
 }
+
+// export default class App extends Component {
+
+//     render() {
+//       return(
+//         <View style = {{flex:1, flexDirection: 'column'}}>
+//             <View style = {{flex:1, justifyContent: 'space-between'}}>
+//                 <Image style = {{flex:1, resizeMode: 'contain', paddingBottom: 10}}
+//                  source = {{uri: 'http://pngimg.com/uploads/fcb_logo/fcb_logo_PNG23.png'}}/>
+//               <Text style = {{fontSize: 14}}>THIS IS THE TITLE. </Text>
+//           <Text style = {{fontSize: 12}}>THIS IS THE DESCRIPTION</Text>
+//             </View>
+//             <View style = {{flex:1 }}>
+//                 <Image style = {{flex:1}} source = {{uri: 'https://www.clipartmax.com/png/middle/232-2321271_manchester-united-fc-old-3-manchester-united-logo-vector.png'}}/>
+//             </View>
+//         </View>
+//       );
+//     }
+
+// }
+
+// class CellComponent extends Component {
+
+//     render() {
+//       return(
+//         <View style = {{flex:1, justifyContent: 'space-between', flexDirection: 'column'}}>
+//             <Image style = {{flex:1, resizeMode: 'contain', paddingBottom: 10}}
+//              source = {{uri: 'http://pngimg.com/uploads/fcb_logo/fcb_logo_PNG23.png'}}/>
+//           <Text style = {{fontSize: 16}}>THIS IS THE TITLE. </Text>
+//           <Text style = {{fontSize: 14}}>THIS IS THE DESCRIPTION</Text>
+//         </View>
+//       );
+//     }
+
+// }
